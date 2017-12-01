447c3a28b464c98a0d25399c3de084b46efea545
Benita <benitazhang@gmail.com>
2017-12-01 15:24:24 -0600

    db returns {} instead of []

--- server/models/stockModel.js
+++ server/models/stockModel.js
@@ -5,7 +5,7 @@ const db = require('../../config/db');
 async function getStockData (ticker) {
     try {
         let result = await db.one('SELECT * FROM stock_price WHERE "ticker" = $1', ticker);
-        return result;
+        return result[0];
     } catch (err) {
         console.log(`${ticker} not in database`);
         return null;
@@ -15,7 +15,7 @@ async function getStockData (ticker) {
 async function saveStockData (stock) {
     try {
         let result = await db.query('INSERT INTO stock_price(ticker, closing_price, current_price, last_updated) VALUES(${ticker}, ${closingPrice}, ${currentPrice}, ${lastUpdated}) RETURNING *', stock);
-        return result;
+        return result[0];
     } catch (err) {
         console.log('saveStockData error:', err)
     }
@@ -24,7 +24,7 @@ async function saveStockData (stock) {
 async function updateStockData (stock) {
     try {
         let result = await db.query('UPDATE stock_price SET closing_price=${closingPrice}, current_price=${currentPrice}, last_updated=${lastUpdated} WHERE ticker=${ticker} RETURNING *', stock);
-        return result;
+        return result[0];
     } catch(err) {
         console.log('updateStockData error:', err);
     }
